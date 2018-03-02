bfe553021d29cb45f2a3a8334439c66792328127
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-03-02 17:03:42 -0600

    another test

--- src/main/java/com/timeclock/web/ClockBeta/ClockBetaApplication.java
+++ src/main/java/com/timeclock/web/ClockBeta/ClockBetaApplication.java
@@ -2,11 +2,24 @@ package com.timeclock.web.ClockBeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ClockBetaApplication {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/rest/**").allowedOrigins("http://localhost:3000, https://spring-clock-ui.herokuapp.com");
+				registry.addMapping("/login").allowedOrigins("http://localhost:3000, https://spring-clock-ui.herokuapp.com");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClockBetaApplication.class, args);
 	}
--- src/main/java/com/timeclock/web/ClockBeta/security/WebSecurityConfig.java
+++ src/main/java/com/timeclock/web/ClockBeta/security/WebSecurityConfig.java
@@ -44,7 +44,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
-                // And filter other requests to check the presence of JWT in header
                 .addFilterBefore(new JWTAuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class);
     }

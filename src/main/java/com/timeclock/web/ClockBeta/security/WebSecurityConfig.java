c841215fab4bd182875a025b384f68fe2c3f71bd
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-03-02 17:08:33 -0600

    yet another test

--- src/main/java/com/timeclock/web/ClockBeta/security/WebSecurityConfig.java
+++ src/main/java/com/timeclock/web/ClockBeta/security/WebSecurityConfig.java
@@ -36,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @CrossOrigin
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
